a92d7608a2d7eeebde7002c2f40d4beac1d816de
Vlad <vldcocos@gmail.com>
2020-04-01 22:37:57 +0300

    updated database init file

--- database/init.sql
+++ database/init.sql
@@ -1,7 +1,9 @@
 CREATE DATABASE IF NOT EXISTS hotel_booking_db;
 use hotel_booking_db;
+DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS rooms;
 
-CREATE TABLE rooms(
+CREATE TABLE IF NOT EXISTS rooms(
     room_id INT(6) UNSIGNED NOT NULL PRIMARY KEY,
     room_type VARCHAR(32),
     capacity INT(6),
@@ -10,14 +12,15 @@ CREATE TABLE rooms(
     price INT(6)
 );
 
-CREATE TABLE bookings(
+CREATE TABLE IF NOT EXISTS bookings(
     booking_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    booked_room_id INT(6) UNSIGNED NOT NULL,
+    room_id INT(6) UNSIGNED NOT NULL,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     check_in DATE,
     check_out DATE,
-    FOREIGN KEY (booked_room_id) REFERENCES rooms(room_id)
+    FOREIGN KEY (room_id) REFERENCES rooms(room_id)
 );
 
-INSERT INTO rooms VALUES (1, "Apartament", 4, True, False, 120);
+INSERT INTO rooms (room_id, room_type, capacity, bathroom, balcony, price) VALUES (1, "Apartament", 4, True, False, 210);
+INSERT INTO rooms (room_id, room_type, capacity, bathroom, balcony, price) VALUES (2, "Double room", 2, True, False, 100);
